4bdf27ddd9d3c28931881fac6be17f6dc2406eae
Gabriel <ga.vidal@duocuc.cl>
2024-06-26 17:29:04 -0400

    logica del programa pasada a funciones

--- utilidades.py
+++ utilidades.py
@@ -3,3 +3,89 @@ menu = ['Ingresar alumno', 'Ingresar nota', 'ingresar Anotacion', 'Consultar Alu
 def consultar_menu():
   for cont, elemento in enumerate(menu, start=1):
     print(cont, '-' ,elemento)
+    
+def registrar_alumno(estudiantes):
+  while True:
+    rut = input('Ingrese el RUT del alumno en formato 123456789 (sin puntos ni guión) para ingresar o modificar un alumno: ')
+    if len(rut) > 8 and rut.isdigit():
+        break
+    else:
+        print('Formato inválido. Ingrese otro.')
+  
+  while True:
+    nombre_apellido = input('Ingrese el nombre y apellido del alumno: ').title()
+    if not nombre_apellido.replace(' ', '').isalpha():
+        print('Valor inválido.')
+    else:
+        break
+  
+  curso = input('Ingrese el curso o sección del alumno: ').upper()
+  
+  alumno = {
+    'rut': rut,
+    'nombre_apellido': nombre_apellido,
+    'curso': curso,
+    'nota': '',
+    'anotaciones': ''
+  }
+  
+  estudiantes.append(alumno)
+  print('Registro de alumno exitoso.')
+
+
+def ingresar_nota(estudiantes):
+  buscar = input('Ingrese el RUT (formato: 123456789) del alumno a consultar: ')
+  encontrado = False
+  
+  for alumno in estudiantes:
+    if alumno['rut'] == buscar:
+      encontrado = True
+      if alumno['nota']:
+          print(f'Notas del alumno {alumno["nombre_apellido"]}:')
+          print(alumno['nota'])
+      else:
+          print(f'No se han registrado notas para el alumno {alumno["nombre_apellido"]}.')
+      
+      nota = input('Ingrese la nota del alumno: ')
+      alumno['nota'] += f'\n{nota}' if alumno['nota'] else nota
+      print(f'Notas del alumno {alumno["nombre_apellido"]}: {alumno["nota"]}')
+      break
+
+  if not encontrado:
+    print('No se ha encontrado el RUT ingresado.')
+    
+def ingresar_anotacion(estudiantes):
+  buscar = input('Ingrese el RUT (formato: 123456789) del alumno a consultar: ')
+  encontrado = False
+  
+  for alumno in estudiantes:
+    if alumno['rut'] == buscar:
+      encontrado = True
+      if alumno['anotaciones']:
+          print(f'Observaciones del alumno {alumno["nombre_apellido"]}:')
+          print(alumno['anotaciones'])
+      else:
+          print(f'No se han registrado anotaciones/observaciones para el alumno {alumno["nombre_apellido"]}.')
+      
+      observacion = input('Ingrese la anotación/observación del alumno: ')
+      alumno['anotaciones'] += f'\n{observacion}' if alumno['anotaciones'] else observacion
+      print(f'Anotaciones/observaciones del alumno {alumno["nombre_apellido"]}: {alumno["anotaciones"]}')
+      break
+  
+  if not encontrado:
+    print('No se ha encontrado el RUT ingresado.')
+
+def mostrar_informacion(estudiantes):
+  buscar = input('Ingrese el RUT (formato 123456789) del alumno a consultar: ')
+  encontrado = False
+  
+  for alumno in estudiantes:
+    if alumno['rut'] == buscar:
+        encontrado = True
+        print(f'Alumno {alumno["nombre_apellido"]}\nRUT: {alumno["rut"]}\nCurso/Sección: {alumno["curso"]}\nNotas: {alumno["nota"]}\nObservaciones/Anotaciones: {alumno["anotaciones"]}')
+        break
+  
+  if not encontrado:
+    print('No se ha encontrado el RUT ingresado.')
+
+print('Bienvenidos a Libro Virtual')
